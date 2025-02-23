01860308deeac52ae42905b77969a70bb73a571f
Nicolas Lima <bigmacnls@gmail.com>
2025-02-22 22:23:16 -0300

    feat: all buttons order and back buttons works

--- src/app/[slug]/menu/components/header.tsx
+++ src/app/[slug]/menu/components/header.tsx
@@ -3,7 +3,7 @@
 import { Restaurant } from "@prisma/client";
 import { ChevronLeftIcon, ScrollTextIcon } from "lucide-react";
 import Image from "next/image";
-import { useRouter } from "next/navigation";
+import { useParams, useRouter } from "next/navigation";
 
 import { Button } from "@/components/ui/button";
 
@@ -12,28 +12,31 @@ interface RestaurantHeaderProps {
 }
 
 const RestaurantHeader = ({ restaurant }: RestaurantHeaderProps) => {
+  const { slug } = useParams<{ slug: string }>();
   const router = useRouter();
   const handleBackClick = () => router.back();
+  const handleOrdersClick = () => router.push(`/${slug}/orders`);
   return (
     <div className="relative h-[250px] w-full">
       <Button
         variant="secondary"
         size="icon"
-        className="absolute left-4 top-4 rounded-full z-50"
+        className="absolute left-4 top-4 z-50 rounded-full"
         onClick={handleBackClick}
       >
         <ChevronLeftIcon />
       </Button>
       <Image
         src={restaurant.coverImgUrl}
-        fill
         alt={restaurant.name}
+        fill
         className="object-cover"
       />
       <Button
         variant="secondary"
         size="icon"
-        className="absolute right-4 top-4 rounded-full z-50"
+        className="absolute right-4 top-4 z-50 rounded-full"
+        onClick={handleOrdersClick}
       >
         <ScrollTextIcon />
       </Button>
--- src/app/[slug]/menu/components/products.tsx
+++ src/app/[slug]/menu/components/products.tsx
@@ -1,4 +1,4 @@
-import { ConsumptionMethod, Product } from "@prisma/client";
+import { Product } from "@prisma/client";
 import Image from "next/image";
 import Link from "next/link";
 import { useParams, useSearchParams } from "next/navigation";
@@ -12,26 +12,27 @@ interface ProductsProps {
 const Products = ({ products }: ProductsProps) => {
   const { slug } = useParams<{ slug: string }>();
   const searchParams = useSearchParams();
-  const consumptionMethod = searchParams.get(
-    "consumptionMethod"
-  ) as ConsumptionMethod;
+  const consumptionMethod = searchParams.get("consumptionMethod");
   return (
-    <div className="space-y-3 px-5 py-3">
+    <div className="space-y-3 px-5">
       {products.map((product) => (
         <Link
           key={product.id}
           href={`/${slug}/menu/${product.id}?consumptionMethod=${consumptionMethod}`}
           className="flex items-center justify-between gap-10 border-b py-3"
         >
-          {/* esquerda */}
+          {/* ESQUERDA */}
           <div>
             <h3 className="text-sm font-medium">{product.name}</h3>
             <p className="line-clamp-2 text-sm text-muted-foreground">
               {product.description}
             </p>
-            <p className="font-bold mt-1">{formatCurrency(product.price)}</p>
+            <p className="pt-3 text-sm font-semibold">
+              {formatCurrency(product.price)}
+            </p>
           </div>
-          {/* direita */}
+
+          {/* DIREITA */}
           <div className="relative min-h-[82px] min-w-[120px]">
             <Image
               src={product.imgUrl}
