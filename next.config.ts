2c6c7dbccb4d3f3a7204d5fad0448954d50e74c1
Nicolas Lima <bigmacnls@gmail.com>
2025-02-22 22:45:49 -0300

    chore: another page redirection method

--- next.config.ts
+++ next.config.ts
@@ -4,14 +4,6 @@ const nextConfig: NextConfig = {
   images: {
     remotePatterns: [{ hostname: "u9a6wmr3as.ufs.sh" }],
   },
-  async rewrites() {
-    return [
-      {
-        source: "/",
-        destination: "/fsw-donalds", // substitua pelo slug de um restaurante específico
-      },
-    ];
-  },
 };
 
 export default nextConfig;
