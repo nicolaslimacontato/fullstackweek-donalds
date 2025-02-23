397950f189830cf7f3ac7fb200aca58d5136379b
Nicolas Lima <bigmacnls@gmail.com>
2025-02-22 22:29:55 -0300

    fix: initial page

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,7 @@
 import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
+  basePath: "/fsw-donalds", // Define o subcaminho base da aplicação
   images: {
     remotePatterns: [{ hostname: "u9a6wmr3as.ufs.sh" }],
   },
